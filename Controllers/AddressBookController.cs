765519ddc664ad2a30c6c4100267792cd91c22c6
Shashank Srivastava <Shashank Srivastava@LAPTOP>
2021-08-19 23:30:38 +0530

    Add project files.

---
+++ Controllers/AddressBookController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using AddressBook.Models;
+
+namespace AddressBook.Controllers
+{
+    [RoutePrefix("Api/AddressBook")]
+    public class AddressBookController : ApiController
+    {
+        StaticData contactList = new StaticData();
+
+        [HttpGet]
+        [Route("contact/{id}")]
+        public IHttpActionResult GetContact(int id)
+        {
+            return (IHttpActionResult)contactList.DefaultList.FirstOrDefault(E => E.Id == id);
+        }
+        [HttpGet]
+        public
+    }
+}
